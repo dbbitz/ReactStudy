c60b78fb5e99f48ceffccc81827a1205d971829f
Daniel Barbosa Rocha <81662636+dbbitz@users.noreply.github.com>
2022-11-08 12:34:11 -0300

    my first commit

---
+++ reactapp/src/pages/home.tsx
@@ -0,0 +1,13 @@
+function App() {
+  
+  return (
+    //Fragment 
+    <>
+    <h1>Olá</h1>
+    <input type="text" placeholder="Digite o nome..." />
+    <button type="button">Adicionar</button>
+    </>
+  )
+}
+
+export default App
